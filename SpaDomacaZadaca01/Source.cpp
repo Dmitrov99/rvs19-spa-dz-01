d04c90215e495765491b39cd88d31c14110afa81
dmitrov99 <domagoj.mitrovic1@gmail.com>
2025-04-05 17:02:34 +0200

    first commit

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,24 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
-	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Animirani cvijet");
+    window.setFramerateLimit(60);
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    Cvijet cvijet(&window);
 
-		window.clear();
-		//cvijet.draw();
-		window.display();
-	}
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-	return 0;
+        window.clear(sf::Color(135, 206, 235));
+        cvijet.draw();
+        window.display();
+    }
+
+    return 0;
 }
